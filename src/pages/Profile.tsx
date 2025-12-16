a16a97c3c222db0642a722246a394229f05c896c
Tae.Garam <drasla@naver.com>
2025-12-16 15:51:24 +0900

    * Profile - 타임라인 완료

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
-import { auth, storage } from "../firebase.ts";
-import { type ChangeEvent, useState } from "react";
+import { auth, db, storage } from "../firebase.ts";
+import { type ChangeEvent, useEffect, useState } from "react";
 import type { TweetType } from "../components/Timeline.tsx";
 import { FaUser } from "react-icons/fa";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import Tweet from "../components/Tweet.tsx";
 
 const Wrapper = styled.div`
     display: flex;
@@ -52,6 +54,8 @@ function Profile() {
     const [avatar, setAvatar] = useState(user?.photoURL);
     const [list, setList] = useState<TweetType[]>([]);
 
+    console.log(user);
+
     const onAvatarChange = async (event: ChangeEvent<HTMLInputElement>) => {
         const { files } = event.target;
         if (!user) return;
@@ -74,6 +78,50 @@ function Profile() {
         }
     };
 
+    const fetchTweets = async () => {
+        // firestore에서 데이터를 검색해오는 명령(query)를 작성
+        // query(콜렉션정보, 검색조건)
+        // orderBy : 생성일자 기준으로 내림차순 정렬
+        // 내림차순 : desc , 오름차순 : asc
+        const tweetsQuery = query(
+            collection(db, "tweets"),
+            // where절은 어떠한 항목에 대한 조건을 걸 때 사용
+            where("userId", "==", user?.uid),
+            orderBy("createdAt", "desc"),
+            // limit는 조회해오는 갯수 제한
+            limit(25)
+        );
+
+        // 작성된 query문으로 실제 요청
+        const snapshot = await getDocs(tweetsQuery);
+
+        // 도착된 데이터를 가공
+        // 실질적으로 도착한 데이터 중 우리가 필요한 내용은 snapshot.docs에 있음 (docs는 Array)
+        // Array 내부 요소는 객체로 존재 item = {
+        //                                     data: data(), -> 이게 우리가 작성한 데이터
+        //                                     id: id, -> 이 요소의 고유한 id
+        //                                   }
+
+        const tweets = snapshot.docs.map(
+            (item) => {
+                const data = item.data();
+                return {
+                    tweet: data.tweet,
+                    createdAt: data.createdAt,
+                    userId: data.userId,
+                    username: data.username,
+                    id: item.id,
+                    photo: data.photo,
+                }
+            }
+        )
+        setList(tweets);
+    };
+
+    useEffect(() => {
+        fetchTweets().then(() => {})
+    }, []);
+
     return (
         <Wrapper>
             <AvatarUpload htmlFor={"avatar"}>
@@ -82,7 +130,11 @@ function Profile() {
             <AvatarInput type={"file"} id={"avatar"} accept={"image/*"} onChange={onAvatarChange} />
             <Name>이름</Name>
 
-            <TweetList>트윗 내용들</TweetList>
+            <TweetList>
+                {list.map((item, index) => {
+                    return <Tweet key={index} item={item}/>
+                })}
+            </TweetList>
         </Wrapper>
     );
 }
