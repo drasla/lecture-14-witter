f109e525eb12986f75a5c387ee8a8e5aaf8002c6
Tae.Garam <drasla@naver.com>
2025-12-16 11:05:26 +0900

    * tweet 삭제 기능 완료

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,6 +1,8 @@
 import type { TweetType } from "./Timeline.tsx";
-import { auth } from "../firebase.ts";
+import { auth, db } from "../firebase.ts";
 import styled from "styled-components";
+import { deleteDoc, doc } from "firebase/firestore";
+import { useNavigate } from "react-router";
 
 type Props = {
     item: TweetType;
@@ -36,14 +38,31 @@ const DeleteBtn = styled.button`
 `;
 
 function Tweet({ item }: Props) {
+    const navigate = useNavigate();
+
     const user = auth.currentUser;
+
+    const onDelete = async () => {
+        // 사용자에게 삭제할건지를 재확인
+        const ok = confirm("정말 이 트윗을 삭제하실건가요?");
+        if (!ok) return;
+
+        try {
+            // 재확인 시 진짜 삭제한다면 firestore에서 삭제 처리
+            await deleteDoc(doc(db, "tweets", item.id));
+            // 이후 새로고침
+            navigate(0);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     return (
         <Wrapper>
             <Username>{item.username}</Username>
             <Text>{item.tweet}</Text>
             {/* 접속한 사용자와, 글 작성자가 동일할 때에는 삭제 버튼을 출력 */}
-            {user?.uid === item.userId && <DeleteBtn>Delete</DeleteBtn>}
+            {user?.uid === item.userId && <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>}
         </Wrapper>
     );
 }
